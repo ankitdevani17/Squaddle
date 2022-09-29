200ddae42a6974f92ed021d62b76ed4b74784f6c
neelpopat242 <neelpopat242@gmail.com>
2022-09-29 14:51:05 +0530

    Profile update

--- frontend/sqfront/src/Components/Project.js
+++ frontend/sqfront/src/Components/Project.js
@@ -1,17 +1,64 @@
-import React from 'react'
+import React, {useEffect} from 'react'
 
-const Project = () => {
+const Project = (props) => {
+    
+    const [edit,setedit] = React.useState(false)
+    const deleteproject = () => {
+        
+        
+    }
+    const editproject = () => {
+        setedit(true)
+    }
+
+    const submitproject = () => {
+        setedit(false) 
+    }
+    const [projectData, setprojectData] = React.useState({
+        id : props.projectitem.id,
+        title: props.projectitem.title,
+        grpsize: props.projectitem.grpsize,
+        desc: props.projectitem.desc,
+        mentor: props.projectitem.mentor,
+        duration: props.projectitem.duration,
+        link: props.projectitem.link,
+        repo: props.projectitem.repo,
+        techstack: props.projectitem.techstack,
+    })
+
+    useEffect(() => {
+      setprojectData(props.projectitem);
+      console.log(props.projectitem);
+    }, [props.projectitem])
+    
+
+
+    const textupdate = (e) => {
+        const { name, value } = e.target
+        setprojectData((prev) => {
+            return {
+                ...prev,
+                [name]: value
+            }
+        })
+        console.log(projectData)
+    }
     return (
         <div>
-            <h2>Project  # </h2>
+            <br />
+            <h2>Project  {projectData.id}
+                <button type="button" onClick={editproject} className="btn btn-secondary mx-3">Edit </button>
+                <button type="button"onClick={deleteproject}   className="btn btn-danger">Delete</button>
+            </h2>
+            {edit && <fieldset  >
             <div className='row'>
                 <div className='col-md-3'>
                     <h6>Title</h6>
-                    <input type='text' className='form-control' placeholder='Title' />
+                    <input type='text' className='form-control' name='title' onChange={textupdate} value={projectData.title}  />
                     <h6>Group Size</h6>
-                    <input type='text' className='form-control' placeholder='Group Size' />
+                    <input type='number' className='form-control' name='grpsize' onChange={textupdate} value={projectData.grpsize} placeholder='Group Size' />
                     <h6>Deployed Link (if any)</h6>
-                    <input type='text' className='form-control' placeholder='Group Size' />
+                    <input type='text' className='form-control' name='link' onChange={textupdate} value={projectData.link} placeholder='Group Size' />
 
 
 
@@ -22,106 +69,113 @@ const Project = () => {
                 <div className='col-md-4'>
                     <h5>Description of Project and Outcomes</h5>
                     <div className="form-floating">
-                        <textarea className="form-control" placeholder="Leave a comment here" id="floatingTextarea2" style={{ height: "100px" }}></textarea>
-                        <label for="floatingTextarea2"> Summary</label>
+                        <textarea className="form-control" name='desc' onChange={textupdate} value={projectData.desc} placeholder="Leave a comment here" id="floatingTextarea2" style={{ height: "100px" }}></textarea>
+                        <label > Summary</label>
                     </div>
                     {/* <div className="col-md-6"> */}
-                        <h6>Github Repo</h6>
-                        <input type='text' className='form-control' placeholder='Github Repository' />
+                    <h6>Github Repo</h6>
+                    <input type='text' className='form-control' name="repo" onChange={textupdate} value={projectData.repo} placeholder='Github Repository' />
 
                     {/* </div> */}
 
                 </div>
                 <div className='col-1'>
-                    
+
                 </div>
                 <div className='col-md-3'>
                     <h6>Mentor of the Project</h6>
-                    <input type='text' className='form-control' placeholder='Title' />
-                    <h6>Duration of Project </h6>
-                    <input type='text' className='form-control' placeholder='Title' />
+                    <input type='text' className='form-control' name='mentor' value={projectData.mentor} onChange={textupdate} placeholder='Title' />
+                    <h6>Duration of Project (months)</h6>
+                    <input type='number' className='form-control' name='duration' value={projectData.duration} onChange={textupdate} placeholder='Title' />
 
-                    
+
                 </div>
                 <h6>Frameworks Used </h6>
-                <div className='row'>
-                    <div className='col-md-3'>
-                        <div className="form-check form-switch">
-                            <input className="form-check-input" type="checkbox" role="switch" id="flexSwitchCheckDefault" />
-                            <label className="form-check-label" for="flexSwitchCheckDefault">React.js</label>
-                        </div>
-                        <div className="form-check form-switch">
-                            <input className="form-check-input" type="checkbox" role="switch" id="flexSwitchCheckDefault" />
-                            <label className="form-check-label" for="flexSwitchCheckDefault">UI Design</label>
-                        </div>
-                        <div className="form-check form-switch">
-                            <input className="form-check-input" type="checkbox" role="switch" id="flexSwitchCheckDefault" />
-                            <label className="form-check-label" for="flexSwitchCheckDefault">PHP</label>
-                        </div>
-                        <div className="form-check form-switch">
-                            <input className="form-check-input" type="checkbox" role="switch" id="flexSwitchCheckDefault" />
-                            <label className="form-check-label" for="flexSwitchCheckDefault">Python</label>
-                        </div>
-                    </div>
-                    <div className='col-md-3'>
-                        <div className="form-check form-switch">
-                            <input className="form-check-input" type="checkbox" role="switch" id="flexSwitchCheckDefault" />
-                            <label className="form-check-label" for="flexSwitchCheckDefault">Angular.js</label>
-                        </div>
-                        <div className="form-check form-switch">
-                            <input className="form-check-input" type="checkbox" role="switch" id="flexSwitchCheckDefault" />
-                            <label className="form-check-label" for="flexSwitchCheckDefault">Node.js</label>
-                        </div>
-                        <div className="form-check form-switch">
-                            <input className="form-check-input" type="checkbox" role="switch" id="flexSwitchCheckDefault" />
-                            <label className="form-check-label" for="flexSwitchCheckDefault">Scala</label>
-                        </div>
-                        <div className="form-check form-switch">
-                            <input className="form-check-input" type="checkbox" role="switch" id="flexSwitchCheckDefault" />
-                            <label className="form-check-label" for="flexSwitchCheckDefault">C++</label>
-                        </div>
-                    </div>
-                    <div className='col-md-3'>
-                        <div className="form-check form-switch">
-                            <input className="form-check-input" type="checkbox" role="switch" id="flexSwitchCheckDefault" />
-                            <label className="form-check-label" for="flexSwitchCheckDefault">React.js</label>
-                        </div>
-                        <div className="form-check form-switch">
-                            <input className="form-check-input" type="checkbox" role="switch" id="flexSwitchCheckDefault" />
-                            <label className="form-check-label" for="flexSwitchCheckDefault">UI Design</label>
-                        </div>
-                        <div className="form-check form-switch">
-                            <input className="form-check-input" type="checkbox" role="switch" id="flexSwitchCheckDefault" />
-                            <label className="form-check-label" for="flexSwitchCheckDefault">PHP</label>
-                        </div>
-                        <div className="form-check form-switch">
-                            <input className="form-check-input" type="checkbox" role="switch" id="flexSwitchCheckDefault" />
-                            <label className="form-check-label" for="flexSwitchCheckDefault">Python</label>
-                        </div>
-                    </div>
-                    <div className='col-md-3'>
-                        <div className="form-check form-switch">
-                            <input className="form-check-input" type="checkbox" role="switch" id="flexSwitchCheckDefault" />
-                            <label className="form-check-label" for="flexSwitchCheckDefault">Angular.js</label>
-                        </div>
-                        <div className="form-check form-switch">
-                            <input className="form-check-input" type="checkbox" role="switch" id="flexSwitchCheckDefault" />
-                            <label className="form-check-label" for="flexSwitchCheckDefault">Node.js</label>
-                        </div>
-                        <div className="form-check form-switch">
-                            <input className="form-check-input" type="checkbox" role="switch" id="flexSwitchCheckDefault" />
-                            <label className="form-check-label" for="flexSwitchCheckDefault">Scala</label>
-                        </div>
-                        <div className="form-check form-switch">
-                            <input className="form-check-input" type="checkbox" role="switch" id="flexSwitchCheckDefault" />
-                            <label className="form-check-label" for="flexSwitchCheckDefault">C++</label>
-                        </div>
-                    </div>
-
-
+                <div className="form">
+                    <input type='text' className='form-control' name='techstack' value={projectData.techstack} onChange={textupdate} placeholder='Tech Stack used' />
                 </div>
-            </div>
+                {/* <div className='row'>
+                    <div className='col-md-3'>
+                        <div className="form-check form-switch">
+                            <input className="form-check-input" type="checkbox" role="switch" id="flexSwitchCheckDefault" />
+                            <label className="form-check-label" for="flexSwitchCheckDefault">React.js</label>
+                        </div>
+                        <div className="form-check form-switch">
+                            <input className="form-check-input" type="checkbox" role="switch" id="flexSwitchCheckDefault" />
+                            <label className="form-check-label" for="flexSwitchCheckDefault">UI Design</label>
+                        </div>
+                        <div className="form-check form-switch">
+                            <input className="form-check-input" type="checkbox" role="switch" id="flexSwitchCheckDefault" />
+                            <label className="form-check-label" for="flexSwitchCheckDefault">PHP</label>
+                        </div>
+                        <div className="form-check form-switch">
+                            <input className="form-check-input" type="checkbox" role="switch" id="flexSwitchCheckDefault" />
+                            <label className="form-check-label" for="flexSwitchCheckDefault">Python</label>
+                        </div>
+                    </div>
+                    <div className='col-md-3'>
+                        <div className="form-check form-switch">
+                            <input className="form-check-input" type="checkbox" role="switch" id="flexSwitchCheckDefault" />
+                            <label className="form-check-label" for="flexSwitchCheckDefault">Angular.js</label>
+                        </div>
+                        <div className="form-check form-switch">
+                            <input className="form-check-input" type="checkbox" role="switch" id="flexSwitchCheckDefault" />
+                            <label className="form-check-label" for="flexSwitchCheckDefault">Node.js</label>
+                        </div>
+                        <div className="form-check form-switch">
+                            <input className="form-check-input" type="checkbox" role="switch" id="flexSwitchCheckDefault" />
+                            <label className="form-check-label" for="flexSwitchCheckDefault">Scala</label>
+                        </div>
+                        <div className="form-check form-switch">
+                            <input className="form-check-input" type="checkbox" role="switch" id="flexSwitchCheckDefault" />
+                            <label className="form-check-label" for="flexSwitchCheckDefault">C++</label>
+                        </div>
+                    </div>
+                    <div className='col-md-3'>
+                        <div className="form-check form-switch">
+                            <input className="form-check-input" type="checkbox" role="switch" id="flexSwitchCheckDefault" />
+                            <label className="form-check-label" for="flexSwitchCheckDefault">React.js</label>
+                        </div>
+                        <div className="form-check form-switch">
+                            <input className="form-check-input" type="checkbox" role="switch" id="flexSwitchCheckDefault" />
+                            <label className="form-check-label" for="flexSwitchCheckDefault">UI Design</label>
+                        </div>
+                        <div className="form-check form-switch">
+                            <input className="form-check-input" type="checkbox" role="switch" id="flexSwitchCheckDefault" />
+                            <label className="form-check-label" for="flexSwitchCheckDefault">PHP</label>
+                        </div>
+                        <div className="form-check form-switch">
+                            <input className="form-check-input" type="checkbox" role="switch" id="flexSwitchCheckDefault" />
+                            <label className="form-check-label" for="flexSwitchCheckDefault">Python</label>
+                        </div>
+                    </div>
+                    <div className='col-md-3'>
+                        <div className="form-check form-switch">
+                            <input className="form-check-input" type="checkbox" role="switch" id="flexSwitchCheckDefault" />
+                            <label className="form-check-label" for="flexSwitchCheckDefault">Angular.js</label>
+                        </div>
+                        <div className="form-check form-switch">
+                            <input className="form-check-input" type="checkbox" role="switch" id="flexSwitchCheckDefault" />
+                            <label className="form-check-label" for="flexSwitchCheckDefault">Node.js</label>
+                        </div>
+                        <div className="form-check form-switch">
+                            <input className="form-check-input" type="checkbox" role="switch" id="flexSwitchCheckDefault" />
+                            <label className="form-check-label" for="flexSwitchCheckDefault">Scala</label>
+                        </div>
+                        <div className="form-check form-switch">
+                            <input className="form-check-input" type="checkbox" role="switch" id="flexSwitchCheckDefault" />
+                            <label className="form-check-label" for="flexSwitchCheckDefault">C++</label>
+                        </div>
+                    </div>
 
+
+                </div> */}
+
+            </div>
+            <br />
+            <button type='submit' onClick={submitproject} className='btn btn-primary'>Submit</button>
+            </fieldset>
+            }
         </div>
     )
 }
--- frontend/sqfront/src/Screen/Profile.js
+++ frontend/sqfront/src/Screen/Profile.js
@@ -5,14 +5,56 @@ import Proj from '../Components/Project'
 const Profile = () => {
 
     const imageupload = () => {
+    }
+
+    const deleteproject = (id) => {
+        console.log(id)
 
     }
+    const addProject = () => {
+        addprojectlist((projectlist) => {
+            return(
+            [
+                ...projectlist, 
+                { 
+                    id: projectlist.length + 1, title: '', grpsize: '', desc: '', mentor: '', duration: '', link: '', repo: '', techstack: '' 
+                }
+            ]
+            )
+        })
+    }
+    
+
+    const [projectlist, addprojectlist] = React.useState([
+        {
+            id : 1,
+            title: 'Project 1',
+            grpsize: '2',
+            desc: 'This is a project',
+            mentor: 'Mentor 1',
+            duration: '2',
+            link: '',
+            repo: '',
+            techstack: '',
+        },
+        // {
+        //     id : 2,
+        //     title: 'Project 1',
+        //     grpsize: '2',
+        //     desc: 'This is a project',
+        //     mentor: 'Mentor 1',
+        //     duration: '2',
+        //     link: '',
+        //     repo: '',
+        //     techstack: '',
+        // },
+
+       
+    ])
+
     return (
         <div>
             <div className='container'>
-
-
-
                 <div className='row'>
                     <div className='col-md-3'>
                         <img src={image} className="image-style align-content-center flex justify-content-center my-4 mx-3" alt="..."></img>
@@ -34,7 +76,7 @@ const Profile = () => {
                         <h5>Short Bio</h5>
                         <div className="form-floating">
                             <textarea className="form-control" placeholder="Leave a comment here" id="floatingTextarea2" style={{ height: "110px" }}></textarea>
-                            <label for="floatingTextarea2"> Short Intro about yourself ...</label>
+                            <label  >Short Intro about yourself </label>
                         </div>
                         <h5>Twitter :  </h5>
                         <input type='text' className='form-control' placeholder='Twitter Url' />
@@ -43,36 +85,45 @@ const Profile = () => {
                 </div>
 
                 <div className='row'>
-                <div className='col-md-3'>
-                    <h5>Area of Interest</h5>
-                    <div className="dropdown">
-                        <button className="btn btn-light dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
-                            Dropdown button
-                        </button>
-                        <ul className="dropdown-menu">
-                            <li><a className="dropdown-item" href="#">Web Development </a></li>
-                            <li><a className="dropdown-item" href="#">App Development</a></li>
-                            <li><a className="dropdown-item" href="#">ML/AI</a></li>
-                            <li><a className="dropdown-item" href="#">DevOps</a></li>
-                            <li><a className="dropdown-item" href="#">Networks</a></li>
-                            <li><a className="dropdown-item" href="#">CP</a></li>
-                        </ul>
+                    <div className='col-md-3'>
+                        <h5>Area of Interest</h5>
+                        <div className="dropdown">
+                            <button className="btn btn-light dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                Dropdown button
+                            </button>
+                            <ul className="dropdown-menu">
+                                <li><a className="dropdown-item" href="#">Web Development </a></li>
+                                <li><a className="dropdown-item" href="#">App Development</a></li>
+                                <li><a className="dropdown-item" href="#">ML/AI</a></li>
+                                <li><a className="dropdown-item" href="#">DevOps</a></li>
+                                <li><a className="dropdown-item" href="#">Networks</a></li>
+                                <li><a className="dropdown-item" href="#">CP</a></li>
+                            </ul>
+                        </div>
+                    </div>
+
+
+                    <div className='col-md-3'>
+                        <h6>Industrial Experience </h6>
+                        <input type='text' className='form-control' placeholder='Title' />
+
                     </div>
                 </div>
-                
 
-                <div className='col-md-3'>
-                    <h6>Industrial Experience </h6>
-                    <input type='text' className='form-control' placeholder='Title' />
+                <h3>Projects
+                    <button type="button" onClick={addProject} className="mx-3 btn btn-primary">Add Project</button>
+                </h3>
+                {
+                    projectlist.map((project) => {
+                        return (
+                                <Proj projectitem={project} key={project.id}  />  
+                        )
+                    }
+                    )
+                }
+                {/* <Proj/> */}
 
-                </div>
-                </div>
 
-                <h5>Projects  : </h5>
-                <Proj />
-                <br />
-                <br />
-                <Proj />
 
 
             </div>
