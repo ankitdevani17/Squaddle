243eecb46e7c47893603523d3da7b89e9202183d
neelpopat242 <neelpopat242@gmail.com>
2022-10-11 18:15:31 +0530

    Profile initialize

--- frontend/sqfront/src/Components/Project.js
+++ frontend/sqfront/src/Components/Project.js
@@ -1,21 +1,10 @@
-import React, {useEffect} from 'react'
+import React, { useEffect, useState } from 'react'
 
 const Project = (props) => {
-    
-    const [edit,setedit] = React.useState(false)
-    const deleteproject = () => {
-        
-        
-    }
-    const editproject = () => {
-        setedit(true)
-    }
+    const [toggle, settoggle] = useState(true)
 
-    const submitproject = () => {
-        setedit(false) 
-    }
-    const [projectData, setprojectData] = React.useState({
-        id : props.projectitem.id,
+    const [projectData, setprojectData] = useState({
+        id: props.projectitem.id,
         title: props.projectitem.title,
         grpsize: props.projectitem.grpsize,
         desc: props.projectitem.desc,
@@ -26,11 +15,15 @@ const Project = (props) => {
         techstack: props.projectitem.techstack,
     })
 
+
     useEffect(() => {
-      setprojectData(props.projectitem);
-      console.log(props.projectitem);
+        console.log(toggle)
+        settoggle(props.t)
+    }, [props.t])
+
+    useEffect(() => {
+        setprojectData(props.projectitem);
     }, [props.projectitem])
-    
 
 
     const textupdate = (e) => {
@@ -45,138 +38,7 @@ const Project = (props) => {
     }
     return (
         <div>
-            <br />
-            <h2>Project  {projectData.id}
-                <button type="button" onClick={editproject} className="btn btn-secondary mx-3">Edit </button>
-                <button type="button"onClick={deleteproject}   className="btn btn-danger">Delete</button>
-            </h2>
-            {
-            <fieldset disabled={!edit}>
-            <div className='row'>
-                <div className='col-md-3'>
-                    <h6>Title</h6>
-                    <input type='text' className='form-control' name='title' onChange={textupdate} value={projectData.title}  />
-                    <h6>Group Size</h6>
-                    <input type='number' className='form-control' name='grpsize' onChange={textupdate} value={projectData.grpsize} placeholder='Group Size' />
-                    <h6>Deployed Link (if any)</h6>
-                    <input type='text' className='form-control' name='link' onChange={textupdate} value={projectData.link} placeholder='Group Size' />
 
-
-
-                </div>
-                <div className='col-1'>
-
-                </div>
-                <div className='col-md-4'>
-                    <h5>Description of Project and Outcomes</h5>
-                    <div className="form-floating">
-                        <textarea className="form-control" name='desc' onChange={textupdate} value={projectData.desc} placeholder="Leave a comment here" id="floatingTextarea2" style={{ height: "100px" }}></textarea>
-                        <label > Summary</label>
-                    </div>
-                    {/* <div className="col-md-6"> */}
-                    <h6>Github Repo</h6>
-                    <input type='text' className='form-control' name="repo" onChange={textupdate} value={projectData.repo} placeholder='Github Repository' />
-
-                    {/* </div> */}
-
-                </div>
-                <div className='col-1'>
-
-                </div>
-                <div className='col-md-3'>
-                    <h6>Mentor of the Project</h6>
-                    <input type='text' className='form-control' name='mentor' value={projectData.mentor} onChange={textupdate} placeholder='Title' />
-                    <h6>Duration of Project (months)</h6>
-                    <input type='number' className='form-control' name='duration' value={projectData.duration} onChange={textupdate} placeholder='Title' />
-
-
-                </div>
-                <h6>Frameworks Used </h6>
-                <div className="form">
-                    <input type='text' className='form-control' name='techstack' value={projectData.techstack} onChange={textupdate} placeholder='Tech Stack used' />
-                </div>
-                {/* <div className='row'>
-                    <div className='col-md-3'>
-                        <div className="form-check form-switch">
-                            <input className="form-check-input" type="checkbox" role="switch" id="flexSwitchCheckDefault" />
-                            <label className="form-check-label" for="flexSwitchCheckDefault">React.js</label>
-                        </div>
-                        <div className="form-check form-switch">
-                            <input className="form-check-input" type="checkbox" role="switch" id="flexSwitchCheckDefault" />
-                            <label className="form-check-label" for="flexSwitchCheckDefault">UI Design</label>
-                        </div>
-                        <div className="form-check form-switch">
-                            <input className="form-check-input" type="checkbox" role="switch" id="flexSwitchCheckDefault" />
-                            <label className="form-check-label" for="flexSwitchCheckDefault">PHP</label>
-                        </div>
-                        <div className="form-check form-switch">
-                            <input className="form-check-input" type="checkbox" role="switch" id="flexSwitchCheckDefault" />
-                            <label className="form-check-label" for="flexSwitchCheckDefault">Python</label>
-                        </div>
-                    </div>
-                    <div className='col-md-3'>
-                        <div className="form-check form-switch">
-                            <input className="form-check-input" type="checkbox" role="switch" id="flexSwitchCheckDefault" />
-                            <label className="form-check-label" for="flexSwitchCheckDefault">Angular.js</label>
-                        </div>
-                        <div className="form-check form-switch">
-                            <input className="form-check-input" type="checkbox" role="switch" id="flexSwitchCheckDefault" />
-                            <label className="form-check-label" for="flexSwitchCheckDefault">Node.js</label>
-                        </div>
-                        <div className="form-check form-switch">
-                            <input className="form-check-input" type="checkbox" role="switch" id="flexSwitchCheckDefault" />
-                            <label className="form-check-label" for="flexSwitchCheckDefault">Scala</label>
-                        </div>
-                        <div className="form-check form-switch">
-                            <input className="form-check-input" type="checkbox" role="switch" id="flexSwitchCheckDefault" />
-                            <label className="form-check-label" for="flexSwitchCheckDefault">C++</label>
-                        </div>
-                    </div>
-                    <div className='col-md-3'>
-                        <div className="form-check form-switch">
-                            <input className="form-check-input" type="checkbox" role="switch" id="flexSwitchCheckDefault" />
-                            <label className="form-check-label" for="flexSwitchCheckDefault">React.js</label>
-                        </div>
-                        <div className="form-check form-switch">
-                            <input className="form-check-input" type="checkbox" role="switch" id="flexSwitchCheckDefault" />
-                            <label className="form-check-label" for="flexSwitchCheckDefault">UI Design</label>
-                        </div>
-                        <div className="form-check form-switch">
-                            <input className="form-check-input" type="checkbox" role="switch" id="flexSwitchCheckDefault" />
-                            <label className="form-check-label" for="flexSwitchCheckDefault">PHP</label>
-                        </div>
-                        <div className="form-check form-switch">
-                            <input className="form-check-input" type="checkbox" role="switch" id="flexSwitchCheckDefault" />
-                            <label className="form-check-label" for="flexSwitchCheckDefault">Python</label>
-                        </div>
-                    </div>
-                    <div className='col-md-3'>
-                        <div className="form-check form-switch">
-                            <input className="form-check-input" type="checkbox" role="switch" id="flexSwitchCheckDefault" />
-                            <label className="form-check-label" for="flexSwitchCheckDefault">Angular.js</label>
-                        </div>
-                        <div className="form-check form-switch">
-                            <input className="form-check-input" type="checkbox" role="switch" id="flexSwitchCheckDefault" />
-                            <label className="form-check-label" for="flexSwitchCheckDefault">Node.js</label>
-                        </div>
-                        <div className="form-check form-switch">
-                            <input className="form-check-input" type="checkbox" role="switch" id="flexSwitchCheckDefault" />
-                            <label className="form-check-label" for="flexSwitchCheckDefault">Scala</label>
-                        </div>
-                        <div className="form-check form-switch">
-                            <input className="form-check-input" type="checkbox" role="switch" id="flexSwitchCheckDefault" />
-                            <label className="form-check-label" for="flexSwitchCheckDefault">C++</label>
-                        </div>
-                    </div>
-
-
-                </div> */}
-
-            </div>
-            <br />
-            <button type='submit' onClick={submitproject} className='btn btn-primary'>Submit</button>
-            </fieldset>
-            }
         </div>
     )
 }
--- frontend/sqfront/src/Screen/Profile.js
+++ frontend/sqfront/src/Screen/Profile.js
@@ -1,33 +1,43 @@
-import React from 'react'
+import React, { useState } from 'react'
 import image from '../Image/avatar.jpeg'
 import Proj from '../Components/Project'
 
 const Profile = () => {
-
+    const [edit, setedit] = useState(false)
     const imageupload = () => {
     }
 
     const deleteproject = (id) => {
-        console.log(id)
+        addprojectlist(projectlist.filter((pro) => pro.id !== id))
+    }
+    const editproject = () => {
+        console.log("hello")
+        console.log(edit)
+        setedit(true)
+    }
 
+    const submitproject = () => {
+        console.log("hello")
+        console.log(edit)
+        setedit(false)
     }
     const addProject = () => {
         addprojectlist((projectlist) => {
-            return(
-            [
-                ...projectlist, 
-                { 
-                    id: projectlist.length + 1, title: '', grpsize: '', desc: '', mentor: '', duration: '', link: '', repo: '', techstack: '' 
-                }
-            ]
+            return (
+                [
+                    ...projectlist,
+                    {
+                        id: (projectlist.length == 0) ? 1 : projectlist[projectlist.length - 1].id + 1, title: '', grpsize: '', desc: '', mentor: '', duration: '', link: '', repo: '', techstack: ''
+                    }
+                ]
             )
         })
     }
-    
 
-    const [projectlist, addprojectlist] = React.useState([
+
+    const [projectlist, addprojectlist] = useState([
         {
-            id : 1,
+            id: 1,
             title: 'Project 1',
             grpsize: '2',
             desc: 'This is a project',
@@ -37,19 +47,17 @@ const Profile = () => {
             repo: '',
             techstack: '',
         },
-        // {
-        //     id : 2,
-        //     title: 'Project 1',
-        //     grpsize: '2',
-        //     desc: 'This is a project',
-        //     mentor: 'Mentor 1',
-        //     duration: '2',
-        //     link: '',
-        //     repo: '',
-        //     techstack: '',
-        // },
-
-       
+        {
+            id: 2,
+            title: 'Project 1',
+            grpsize: '2',
+            desc: 'This is a project',
+            mentor: 'Mentor 1',
+            duration: '2',
+            link: '',
+            repo: '',
+            techstack: '',
+        },
     ])
 
     return (
@@ -106,25 +114,70 @@ const Profile = () => {
                     <div className='col-md-3'>
                         <h6>Industrial Experience </h6>
                         <input type='text' className='form-control' placeholder='Title' />
-
                     </div>
                 </div>
 
                 <h3>Projects
                     <button type="button" onClick={addProject} className="mx-3 btn btn-primary">Add Project</button>
                 </h3>
-                {
-                    projectlist.map((project) => {
+                {/* {
+                    projectlist.map((project, ind, edit) => {
                         return (
-                                <Proj projectitem={project} key={project.id}  />  
+                            <>
+                                <h2>Project {ind + 1}
+                                    <button type="button" onClick={editproject} className="btn btn-secondary mx-3">Edit </button>
+                                    <button type="button" onClick={() => deleteproject(project.id)} className="btn btn-danger">Delete</button>
+                                </h2>
+                                <br />
+                                {
+                                    <fieldset disabled={!toggle}>
+                                        <div className='row'>
+                                            <div className='col-md-3'>
+                                                <h6>Title</h6>
+                                                <input type='text' className='form-control' name='title' onChange={textupdate} value={projectData.title} />
+                                                <h6>Group Size</h6>
+                                                <input type='number' className='form-control' name='grpsize' onChange={textupdate} value={projectData.grpsize} placeholder='Group Size' />
+                                                <h6>Deployed Link (if any)</h6>
+                                                <input type='text' className='form-control' name='link' onChange={textupdate} value={projectData.link} placeholder='Group Size' />
+                                            </div>
+                                            <div className='col-1'>
+                                            </div>
+                                            <div className='col-md-4'>
+                                                <h5>Description of Project and Outcomes</h5>
+                                                <div className="form-floating">
+                                                    <textarea className="form-control" name='desc' onChange={textupdate} value={projectData.desc} placeholder="Leave a comment here" id="floatingTextarea2" style={{ height: "100px" }}></textarea>
+                                                    <label > Summary</label>
+                                                </div>
+
+                                                <h6>Github Repo</h6>
+                                                <input type='text' className='form-control' name="repo" onChange={textupdate} value={projectData.repo} placeholder='Github Repository' />
+                                            </div>
+                                            <div className='col-1'>
+                                            </div>
+                                            <div className='col-md-3'>
+                                                <h6>Mentor of the Project</h6>
+                                                <input type='text' className='form-control' name='mentor' value={projectData.mentor} onChange={textupdate} placeholder='Title' />
+                                                <h6>Duration of Project (months)</h6>
+                                                <input type='number' className='form-control' name='duration' value={projectData.duration} onChange={textupdate} placeholder='Title' />
+
+
+                                            </div>
+                                            <h6>Frameworks Used </h6>
+                                            <div className="form">
+                                                <input type='text' className='form-control' name='techstack' value={projectData.techstack} onChange={textupdate} placeholder='Tech Stack used' />
+                                            </div>
+
+
+                                        </div>
+                                        <br />
+                                    </fieldset>
+                                }
+                                <button type='submit' onClick={submitproject} className='btn btn-primary' disabled={edit}>Submit</button >
+                            </>
                         )
                     }
                     )
-                }
-                {/* <Proj/> */}
-
-
-
+                } */}
 
             </div>
         </div>
