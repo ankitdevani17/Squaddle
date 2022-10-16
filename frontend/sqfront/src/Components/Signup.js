2bb745e4c9f0d970f7b4d20fe17bae971b5334d0
neelpopat242 <neelpopat242@gmail.com>
2022-10-16 16:58:37 +0530

    Signup user working

--- frontend/sqfront/src/Components/Signup.js
+++ frontend/sqfront/src/Components/Signup.js
@@ -1,17 +1,20 @@
-import React,{useState} from 'react'
+import axios from 'axios'
+import React, { useState } from 'react'
 
 const Signup = () => {
-    const [name,setName] = useState('')
+    const [name, setName] = useState('')
     const [email, setEmail] = useState('')
     const [password, setPassword] = useState('')
     const [password2, setPassword2] = useState('')
+    const [signupsuccess, setsignupsuccess] = useState(false)
+    const [signupfail, setsignupfail] = useState(false)
 
     const textupdate = (e) => {
         const { name, value } = e.target
-        if(name === 'name'){
+        if (name === 'name') {
             console.log(value)
             setName(value)
-            
+
         }
         if (name === 'email') {
             console.log(value)
@@ -26,53 +29,74 @@ const Signup = () => {
             setPassword2(value)
         }
     }
-    
-    const signup= ((e) => { 
-        e.preventdefault()
-        if(password){
-            if(password === password2){
+
+    const signup = ((e) => {
+        e.preventDefault()
+        if (password) {
+            if (password === password2) {
                 console.log("Password matches")
+                axios.post("http://localhost:4000/api/v1/register", {
+                    name: name,
+                    email: email,
+                    password: password
+                },
+                    { mode: 'cors' },
+                    { withCredentials: true }
+                )
+                    .then(() => {
+                        console.log("login successful")
+                        setsignupsuccess(true)
+                        setsignupfail(false)
+                    })
+                    .catch(() => {
+                        setsignupsuccess(false)
+                        setsignupfail(true)
+                    })
+
+
             }
         }
-        
-        // 
-        // fetch('http://localhost:4000/api/v1/register', {
-        //     method: 'POST' , mode : 'cors',
-        //     headers : {
-        //         'Content-Type': 'application/json'
-        //     },
-        //     body : JSON.stringify({
-        //         name : name,
-        //         email : email,
-        //         password : password
-        //     })
-        // })
+
+
+
 
     })
 
     return (
         <div>
+            {
+                signupsuccess &&
+                <div className="alert alert-success" role="alert">
+                    Login Successful
+                </div>
+            }
+            {
+                signupfail &&
+                <div className="alert alert-danger" role="alert">
+                    Login Failed
+                </div>
+            }
             <form>
                 <h1> SIgnup</h1>
-            <div className="mb-3">
-                    <label  className="form-label">Name</label>
+                <div className="mb-3">
+                    <label className="form-label">Name</label>
                     <input type="text" className="form-control" id="exampleInputName" onChange={textupdate} name="name" value={name} aria-describedby="emailHelp" />
-                    
+
                 </div>
                 <div className="mb-3">
-                    <label  className="form-label">Email address</label>
+                    <label className="form-label">Email address</label>
                     <input type="email" className="form-control" id="exampleInputEmail1" name="email" value={email} onChange={textupdate} aria-describedby="emailHelp" />
                     <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div>
                 </div>
                 <div className="mb-3">
-                    <label  className="form-label">Password</label>
+                    <label className="form-label">Password</label>
                     <input type="password" className="form-control" name="password" value={password} onChange={textupdate} id="exampleInputPassword1" />
                 </div>
                 <div className="mb-3">
-                    <label  className="form-label">Re-enter Password</label>
+                    <label className="form-label">Re-enter Password</label>
                     <input type="password" className="form-control" name="password2" value={password2} onChange={textupdate} id="exampleInputPassword2" />
                 </div>
-                <button type='submit'  onClick={signup} className="btn btn-dark">Create Account</button>
+                <button type='button' onClick={signup} className="btn btn-dark">Create Account</button>
             </form>
         </div>
     )
