56dd5cacc5c2d82b04923b2a4153d5afbc428ce0
kashishjivani <kashishjivani2002@gmail.com>
2022-10-17 14:56:24 +0530

    Home Updates

--- frontend/sqfront/src/Components/Card.js
+++ frontend/sqfront/src/Components/Card.js
@@ -1,35 +1,57 @@
-import React from 'react'
-import avatar from '../Image/avatar.jpeg'
-import {BsSuitHeartFill} from 'react-icons/bs'
-import {ImCross} from 'react-icons/im'
+import React from "react";
+import avatar from "../Image/avatar.jpeg";
+import ReactRoundedImage from "react-rounded-image";
+import "./Card.css";
+import { BsSuitHeartFill } from "react-icons/bs";
+import { ImCross } from "react-icons/im";
 
 const Card = () => {
-
   return (
     <div>
-        <div className="card" >
-            <div className="card-body text-center">
-                <img className="rounded"src={avatar}/>
-                <h5 className="card-title">Name : Shyam Ram </h5>
-                <h5 className="card-title">University :Shyam Ram </h5>
-                <h5 className="card-title">Area of Interest : Testing </h5>
-                <h5 className="card-title">Performed Projects : </h5>
-                <h5 className="card-title">1. Title</h5>
-                <h5 className="card-title">Description</h5>
-                <h5 className="card-title">Tech Stack</h5>
-                <h5 className="card-title">2. Title</h5>
-                <h5 className="card-title">Description</h5>
-                <h5 className="card-title">Tech Stack</h5>
-                <h5 className="card-title">Linkedin</h5>
-                <h5 className="card-title">YOE : 2 yrs</h5>
+      <div className="card">
+        <div className="card-body text-center">
+          {/* <img className="rounded"src={avatar}/> */}
+          <div className="image">
+            <ReactRoundedImage
+              image={avatar}
+              roundedColor="#321124"
+              imageWidth="150"
+              imageHeight="150"
+              roundedSize="13"
+              borderRadius="70"
+              hoverColor="#DD1144"
+            />
+          </div>
+          <br></br>
+          <h5 className="card-title">Deepak Ahuja </h5>
+          <h5 className="card-title">Ahmedabad University </h5>
+          <h5 className="card-title">React JS, Node JS </h5>
+          <h5 className="card-title">Projects </h5>
 
-                <button type='button' className='btn btn-warning rounded  '> <ImCross/></button>
-                <button type='button' className='btn btn-warning rounded '> <BsSuitHeartFill/></button>
-                
-            </div>
+          <div>
+          <h5 className="card-title">1. Title</h5>
+          <h5 className="card-title">Description</h5>
+          <h5 className="card-title">Tech Stack</h5>
+          </div>
+          <h5 className="card-title">2. Title</h5>
+          <h5 className="card-title">Description</h5>
+          <h5 className="card-title">Tech Stack</h5>
+          <h5 className="card-title">Linkedin</h5>
+          <h5 className="card-title">YOE: 2 yrs</h5>
+
+          <button type="button">
+            {" "}
+            <ImCross />
+          </button>
+          <button type="button" className="btn btn-warning rounded ">
+            {" "}
+            <BsSuitHeartFill />
+          </button>
         </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Card
+//className="btn btn-warning rounded
+export default Card;
