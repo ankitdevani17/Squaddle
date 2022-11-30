de53a692f514d848cf27e7cd22711ebae373a752
neelpopat242 <neelpopat242@gmail.com>
2022-11-30 19:02:47 +0530

    Click user email setting

--- frontend/sqfront/src/Components/Conversation.js
+++ frontend/sqfront/src/Components/Conversation.js
@@ -1,11 +1,20 @@
 import React from "react";
 import "./Conversation.css";
 
-function Conversation({ pic, curruser }) {
+function Conversation({ pic, curruser , toemailuser, settoemailuser}) {
+  
   return (
-    <div className="Conversation">
+    <div className="Conversation" onClick ={ ()=>{
+      if(curruser?.email){
+      settoemailuser(curruser?.email)
+      
+    }
+    }}>
+      {/* {
+        console.log("hello ")
+      } */}
       <img src={pic} alt="Profile" className="Conversation_Img" />
-      <span className="Conversation_Name">{curruser}</span>
+      <span className="Conversation_Name">{curruser.name}</span>
     </div>
   );
 }
--- frontend/sqfront/src/Screen/Message.js
+++ frontend/sqfront/src/Screen/Message.js
@@ -1,6 +1,6 @@
-import React, { useState,useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import "./Message.css";
-import axios from "axios"
+import axios from "axios";
 
 import Conversation from "../Components/Conversation";
 import Text from "../Components/Text";
@@ -8,19 +8,28 @@ import ChatOnline from "../Components/ChatOnline";
 import photo from "../Image/about-bg1.jpeg";
 import photo1 from "../Image/avatar.jpeg";
 import { useCookies } from "react-cookie";
-
-
+import { useDispatch, useSelector } from "react-redux";
+import { authActions } from "../store/auth-slice";
 
 function Message() {
-  const getMessage = async ()=>{
-   
-    // setCurrConvo(res.data)
-    // console.log(res)
-  }
+  const userlog = useSelector((state) => state.auth.user);
+  const [curruser, setcurruser] = useState([]);
 
+  const [toemailuser, settoemailuser] = useState("");
+  useEffect(() => {
+    axios
+      .get(`http://localhost:4000/api/v1/userinfo?email=${userlog.email}`)
+      .then((res) => {
+        if (res.data) {
+          setcurruser(res.data.matches);
+          console.log(res.data.matches);
+        }
+      });
+  }, []);
   const [cookies, setCookie, removeCookie] = useCookies(["user"]);
-  const [currentreceivinguser, setcurrentreceivinguser] = React.useState("Ankit");
-const[convo,setCurrConvo]= React.useState([])
+  // const [currentreceivinguser, setcurrentreceivinguser] =
+  //   React.useState("Ankit");
+  const [convo, setCurrConvo] = React.useState([]);
   const [conversations, setConversations] = React.useState([
     {
       from_email: "a@g.co",
@@ -47,62 +56,59 @@ const[convo,setCurrConvo]= React.useState([])
       timestamp: "2024-11-11T18:42:00.339+00:00",
     },
   ]);
-  const currentchattinguser = (e) => {
-    setcurrentreceivinguser(e.target.value);
-  };
+  // const currentchattinguser = (e) => {
+  //   setcurrentreceivinguser(e.target.value);
+  // };
 
-  useEffect (()=>{
-    axios
-    .get(`http://localhost:4000/api/v1/message`,{
-      from_email : "a@g.co",
-      to_email:"ankitdevani17@gmail.com"
-    })
-    .then((res) => {
-      if (res.data) {
-        // setcurruser(res.data);
-        console.log(res)
-      }
-    })
-    .catch((err) => {
-      // console.log(err)
-    });
-    },[])
+  // useEffect(() => {
+  //   axios
+  //     .get(
+  //       `http://localhost:4000/api/v1/message?from_email=a@g.co&to_email=${toemailuser}`,
+  //       { withCredentials: true, mode: "cors" }
+  //     )
+  //     .then((res) => {
+  //       console.log(res.data);
+  //       // setCurrConvo(res.data)
+  //     });
+  // }, []);
+
   return (
     <>
       <div className="message">
         <div className="chatMenu">
           <div className="chatMenuWrapper">
             <input placeholder="Search For Friends" className="chatMenuInput" />
-            <Conversation
-              onClick={currentchattinguser}
-              curruser={currentreceivinguser}
-              pic={photo}
-            />
-             <button className="chatSubmitButton" onClick = {getMessage}> Chat with Ankit</button>
+            {
+              console.log(toemailuser)
+            }
+            {
+            curruser.map((user) => {
+              return (
+                <>
+                  <Conversation
+                    toemailuser={toemailuser}
+                    settoemailuser={settoemailuser}
+                    curruser={user}
+                  />
+                </>
+              );
+            })}
           </div>
         </div>
         <div className="chatBox">
           <div className="chatBoxWrapper">
             <div className="chatBoxTop">
-              {conversations
-
-                // .sort((item) => {
-                //   return item.sort((x, y) => {
-                //     return new Date(x.timestamp) < new Date(y.timestamp) ? 1 : -1
-                //   })
-                // })
-
-                .map((item) => {
-                  return (
-                    <>
-                      {cookies.email === item.to_email ? (
-                        <Text own={true} message={item.message} />
-                      ) : (
-                        <Text message={item.message} />
-                      )}
-                    </>
-                  );
-                })}
+              {conversations.map((item) => {
+                return (
+                  <>
+                    {cookies.email === item.to_email ? (
+                      <Text own={true} message={item.message} />
+                    ) : (
+                      <Text message={item.message} />
+                    )}
+                  </>
+                );
+              })}
               {}
             </div>
             <div className="chatBoxBottom">
@@ -115,10 +121,7 @@ const[convo,setCurrConvo]= React.useState([])
           </div>
         </div>
         <div className="chatOnline">
-          <div className="chatOnlineWrapper">
-            {/* <ChatOnline Photo={photo} /> */}
-            {/* <ChatOnline Photo={photo1} /> */}
-          </div>
+          <div className="chatOnlineWrapper"></div>
         </div>
       </div>
     </>
