a31228f8456b7bda487c0a5916de2d3023097c10
ankit <devaniankit1990@gmail.com>
2022-12-08 17:22:37 +0530

    search functionality in chat

--- frontend/sqfront/src/Components/newCard.js
+++ frontend/sqfront/src/Components/newCard.js
@@ -3,3 +3,40 @@ import {useEffect, useState} from 'react'
 
 import {useCookies} from 'react-cookie'
 import axios from 'axios'
+
+
+
+
+
+const newCard = () => {
+  return (
+   <>
+               <div className="dashboard">
+                <ChatContainer user={user}/>
+                <div className="swipe-container">
+                    <div className="card-container">
+
+                        {filteredGenderedUsers?.map((genderedUser) =>
+                            <TinderCard
+                                className="swipe"
+                                key={genderedUser.user_id}
+                                onSwipe={(dir) => swiped(dir, genderedUser.user_id)}
+                                onCardLeftScreen={() => outOfFrame(genderedUser.first_name)}>
+                                <div
+                                    style={{backgroundImage: "url(" + genderedUser.url + ")"}}
+                                    className="card">
+                                    <h3>{genderedUser.first_name}</h3>
+                                </div>
+                            </TinderCard>
+                        )}
+                        <div className="swipe-info">
+                            {lastDirection ? <p>You swiped {lastDirection}</p> : <p/>}
+                        </div>
+                    </div>
+                </div>
+            </div>
+   </>
+  )
+}
+
+export default newCard
